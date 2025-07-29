d4dbb4fd47d6ca72087696c5678f65f24a40f0bd
Sávio Gomes da Silva <saviogomesdasilvadev@gmail.com>
2025-07-29 16:13:22 -0300

    instanciando classes e criando concatenações

--- src/estudojava/classes/Aluno.java
+++ src/estudojava/classes/Aluno.java
@@ -1,16 +1,18 @@
 package estudojava.classes;
 
-public class Aluno { /*objeto*/
-	String nome;
-	int idade;
-	String dataNascimento;
-	String localNascimento;  // atributos e parametros
-	String matricula;
-	String serieMatriculada;
-	String nomeEscola;
-	String dataMatricula;
-	String nomeMae;
-	String nomePai;
+public class Aluno { /*objeto (nome da classe) */
+	// aqui são os atributos da classe
+	// por padrao o java deixa os atributos privados tem que deixar public
+	public String nome;
+	public int idade;
+	public String dataNascimento;
+	public String localNascimento;  // atributos e parametros
+	public String matricula;
+	public String serieMatriculada;
+	public String nomeEscola;
+	public String dataMatricula;
+	public String nomeMae;
+	public String nomePai;
 
 	public Aluno(String nomePadrao) { // aqui serve para criar os dados na memoria
 		nome = nomePadrao;
@@ -19,4 +21,5 @@ public class Aluno { /*objeto*/
 		nome = nomePadrao;
 		idade = idadePadrao;
 	}
+
 }
--- src/estudojava/executavel/PrimeiraClasse.java
+++ src/estudojava/executavel/PrimeiraClasse.java
@@ -5,16 +5,21 @@ import estudojava.classes.Aluno;
 public class PrimeiraClasse {
 
 	public static void main(String[] args) {
-		
-		Aluno aluno1 =new Aluno();
-		
-		/* Agora aqui tem um objeto real*/
-		Aluno aluno2 = new Aluno();		
-		Aluno aluno3 = new Aluno();
+		// new aluno é uma instância (criação de objeto)
+
+		/* aluno1 é uma referencia para o objeto aluno **/
+		Aluno aluno1 = new Aluno("");
+		aluno1.nome = "João";
+		aluno1.idade = 15;
+		System.out.println(" Nome do deste aluno é = " + aluno1.nome); // tipos de concatenação
+		System.out.println(" O aluno a cima " + "(" + aluno1.nome + ")" + " que foi selecionado tem a idade de " + aluno1.idade
+				+ " anos de idade ");
+		/* Agora aqui tem um objeto real */
+		Aluno aluno2 = new Aluno(null);
+		Aluno aluno3 = new Aluno(null);
 		Aluno aluno4 = new Aluno("Maria");
-		Aluno aluno5 = new Aluno("José",50);
-		
-		
+		Aluno aluno5 = new Aluno("José", 50);
+
 	}
 
 }
